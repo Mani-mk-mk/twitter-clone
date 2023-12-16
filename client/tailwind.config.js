ff3451b5cb2165d1ec2c59018771b128f4bc01c8
Manigandan Kasimani <manigandankasimani@gmail.com>
2023-12-17 00:35:09 +0530

    Updated config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: ["class", '[data-mode="dark"], className'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class", '[data-mode="dark"]', "className"],
   theme: {
     backgroundColor: {
       light: "white",
-      dark: "rgba(0, 0, 0, 0.0)",
+      dark: "black",
       "accent-dark": "#16181C",
       "hover-dark": "#080808",
       "btn-dark": "#1D9BF0",
