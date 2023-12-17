d8fde210e2f09de389e27c3762e65100210a1dc8
Manigandan Kasimani <manigandankasimani@gmail.com>
2023-12-17 22:52:15 +0530

    Use one prop for styling instead of multiple

--- client/src/components/Buttons.tsx
+++ client/src/components/Buttons.tsx
@@ -3,15 +3,14 @@ import { ReactElement } from "react";
 interface ButtonProps {
   icon?: ReactElement;
   text: string;
-  backgroundColor: string;
-  textColor: string;
+  additionalStyles?: string;
 }
 
 const Buttons = (props: ButtonProps) => {
   return (
     <div className="flex w-full justify-start">
       <button
-        className={`${props.backgroundColor} ${props.textColor} text-md flex w-[85%] items-center justify-center gap-2 rounded-full px-8 py-2 font-medium md:w-[60%]`}
+        className={`${props.additionalStyles}  text-md flex w-[85%] items-center justify-center gap-2 rounded-full px-8 py-2 font-medium md:w-[60%]`}
       >
         {props.icon && <span className="h-4 w-4">{props.icon}</span>}
         <span>{props.text}</span>
