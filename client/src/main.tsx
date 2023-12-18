c8b41d0ac07aa0541719242dc2f4fa1397cb608f
Manigandan Kasimani <manigandankasimani@gmail.com>
2023-12-19 00:45:03 +0530

    Use router and routes for easy deployment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,27 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/Register.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div data-mode="dark">
-        <Register />
-      </div>
-    ),
-  },
-  {
-    path: "/home",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div data-mode="dark">
+              <Register />
+            </div>
+          }
+        />
+        <Route path="/home" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 );
