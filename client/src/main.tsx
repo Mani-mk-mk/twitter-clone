cd4704eb625c00a76c20f10c17c3ecfb22a692c9
Manigandan Kasimani <manigandankasimani@gmail.com>
2023-12-24 18:29:35 +0530

    Updat browserRouter to hashRouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/Register.tsx";
 import Home from "./pages/Home.tsx";
 
